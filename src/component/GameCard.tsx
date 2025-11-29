a65c1c7f5ae9e301076c497828df0a5c86558391
RumeshChathuranga <rumarumesh1220@gmail.com>
2025-11-29 16:26:11 +0530

    Display platform icons

--- src/component/GameCard.tsx
+++ src/component/GameCard.tsx
@@ -1,20 +1,20 @@
-import type { Game } from "../hooks/useGames"
-import { Button, Card, Image, Text } from "@chakra-ui/react"
+import { Card, Image, Text } from "@chakra-ui/react";
+import { PlatformIconList } from "./PlatformIconList";
+import type { Game } from "../hooks/useGames";
 
-
-interface Props{
-    game: Game
+interface Props {
+  game: Game;
 }
-export const GameCard = ({game}: Props) => {
+export const GameCard = ({ game }: Props) => {
   return (
     <Card.Root maxW="sm" overflow="hidden" borderRadius={"10px"}>
-      <Image
-        src={game.background_image}
-      />
+      <Image src={game.background_image} />
       <Card.Body gap="2">
         <Card.Title fontSize="2xl">{game.name}</Card.Title>
+        <PlatformIconList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
       </Card.Body>
-      
     </Card.Root>
-  )
-}
+  );
+};
