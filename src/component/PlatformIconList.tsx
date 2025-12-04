9d225a6bd391fe839b70c422164980f15c5424c0
RumeshChathuranga <rumarumesh1220@gmail.com>
2025-12-04 16:34:01 +0530

    Refactor PlatformIconList to conditionally render icons

--- src/component/PlatformIconList.tsx
+++ src/component/PlatformIconList.tsx
@@ -33,13 +33,13 @@ export const PlatformIconList = ({ platforms }: Props) => {
   };
   return (
     <HStack marginY={2}>
-      {platforms.map((platform) => (
-        <Icon
-          key={platform.id}
-          as={iconMap[platform.slug]}
-          color={"gray.500"}
-        />
-      ))}
+      {platforms.map((platform) => {
+        const IconComponent = iconMap[platform.slug];
+        // Only render icon if we have a matching component
+        return IconComponent ? (
+          <Icon key={platform.id} as={IconComponent} color={"gray.500"} />
+        ) : null;
+      })}
     </HStack>
   );
 };
