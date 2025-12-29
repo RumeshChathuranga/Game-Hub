d6ba764cf4aef4b2c1ab0c611a5b2e7c0a23e5b4
RumeshChathuranga <Rumarumesh1220@gmail.com>
2025-12-30 02:15:31 +0530

    Build game trailer

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,5 +1,6 @@
 import ExpandableText from "@/component/ExpandableText";
 import GameAttributes from "@/component/GameAttributes";
+import GameTrailer from "@/component/GameTrailer";
 import useGame from "@/hooks/useGame";
 import { Heading, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
@@ -16,6 +17,7 @@ const GameDetailPage = () => {
       <Heading>{game?.name}</Heading>
       <ExpandableText>{game.description_raw || ""}</ExpandableText>
       <GameAttributes game={game} />
+      <GameTrailer gameId={game.id} />
     </>
   );
 };
