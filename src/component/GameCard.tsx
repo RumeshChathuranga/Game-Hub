e12590b9bbdaf80c2455eb8c020e15dd0cd35d86
RumeshChathuranga <Rumarumesh1220@gmail.com>
2025-12-29 23:17:44 +0530

    Fetch game details

--- src/component/GameCard.tsx
+++ src/component/GameCard.tsx
@@ -4,6 +4,7 @@ import type { Game } from "../hooks/useGames";
 import { CriticScore } from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
 import { Emoji } from "./Emoji";
+import { Link } from "react-router-dom";
 
 interface Props {
   game: Game;
@@ -20,7 +21,8 @@ export const GameCard = ({ game }: Props) => {
           <CriticScore score={game.metacritic} />
         </HStack>
         <Card.Title fontSize="2xl">
-          {game.name}
+          <Link to={"/games/" + game.slug}> {game.name}</Link>
+
           <Emoji rating={game.rating} />
         </Card.Title>
       </Card.Body>
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,5 +1,20 @@
+import useGame from "@/hooks/useGame";
+import { Heading, Spinner, Text } from "@chakra-ui/react";
+import { useParams } from "react-router-dom";
+
 const GameDetailPage = () => {
-  return <div>GameDetailPage</div>;
+  const { slug } = useParams();
+  const { data: game, isLoading, error } = useGame(slug!); // by appending ! we are telling typescript that slug will never be undefined or null
+
+  if (isLoading) return <Spinner />;
+
+  if (error) throw error; // can log permenent place to log errors
+  return (
+    <>
+      <Heading>{game?.name}</Heading>
+      <Text> {game?.description_raw}</Text>
+    </>
+  );
 };
 
 export default GameDetailPage;
